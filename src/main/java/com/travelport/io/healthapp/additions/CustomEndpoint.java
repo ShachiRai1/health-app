6998fb79bc3cd623d2c857abe111e94028e143e2
Singavarapu, Krupa <krupa.singavarapu@C02ST675FVH4-23163.local>
2018-09-27 20:31:24 -0600

    first commit

---
+++ src/main/java/com/travelport/io/healthapp/additions/CustomEndpoint.java
@@ -0,0 +1,48 @@
+package com.travelport.io.healthapp.additions;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CustomEndpoint implements Endpoint{
+    public String getId() {
+        return "customEndpoint";
+    }
+
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public boolean isSensitive() {
+        return true;
+    }
+
+    public List<String> invoke() {
+// Custom logic to build the output
+        List<String> messages = new ArrayList<String>();
+        messages.add("This is message 1");
+        messages.add("This is message 2");
+        return messages;
+    }
+
+    @Override
+    public String id() {
+        return null;
+    }
+
+    @Override
+    public boolean enableByDefault() {
+        return false;
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return null;
+    }
+}
+
+
